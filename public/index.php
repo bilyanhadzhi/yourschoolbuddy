ac1ad8e9ad46042e9a9143cfc20838066a495b0f
Bilyan Hadzhi <bilyanhadzhi@gmail.com>
2017-06-28 23:18:34 +0300

    chore: Test heroku with new index.php

--- public/index.php
+++ public/index.php
@@ -1,14 +1,3 @@
-<?php require_once('../config/config.php') ?>
-<?php require_once(SRC_DIR . '/routing/router.php') ?>
-<?php require_once(SRC_DIR . '/database/database.php') ?>
-
 <?php
-  session_start();
-
-  $url = $_GET['url'] ?? '';
-
-  $router = new Router;
-
-  $router->set_current_url($url);
-  $router->get_from_current_url();
+  echo 'Hey';
 ?>
