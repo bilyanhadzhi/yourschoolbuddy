c68829ce44ae3ab836c69d3b9f28365835c5e439
Bilyan Hadzhi <bilyanhadzhi@gmail.com>
2017-07-05 23:46:38 +0300

    Start working on timer

--- public/js/index.js
+++ public/js/index.js
@@ -11,17 +11,75 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   if (studyBtn) {
-    var timerContainer = document.getElementById('timer');
+    var timerContainer = document.getElementById('timer-container');
     var closeTimerBtn = document.getElementById('close-timer-btn');
+    var studySubject = document.getElementById('study-subject');
+    var timer = document.getElementById('timer');
+
+    var timerBtns = {
+      startPauseBtn: document.getElementById('timer-start-pause-btn'),
+      stopBtn: document.getElementById('timer-stop-btn'),
+    };
 
     studyBtn.addEventListener('click', function() {
-      timerContainer.style.display = 'block';
-      timerContainer.classList.remove('hide');
+      showTimer();
     });
 
     closeTimerBtn.addEventListener('click', function() {
-      timerContainer.style.display = 'hidden';
-      timerContainer.classList.add('hide');
+      hideTimer();
     });
+
+    timerBtns.startPauseBtn.addEventListener('click', function() {
+      if (studySubject.value === "") {
+        console.log('No subject selected');
+      } else {
+        startCountdown(timer);
+        triggerStartBtnLabel(timerBtns.startPauseBtn);
+      }
+    });
+
+    timerBtns.stopBtn.addEventListener('click', function() {
+      console.log('Stop!!');
+      // TODO
+    });
+  }
+
+  function showTimer() {
+    timerContainer.style.display = 'block';
+    timerContainer.classList.remove('hide');
+  }
+
+  function hideTimer() {
+    timerContainer.style.display = 'hidden';
+    timerContainer.classList.add('hide');
+  }
+
+  function countdown(timeStr) {
+    var timeArr = timeStr.split(':');
+
+    var secondsLeft = parseInt(timeArr[0] * 60 + Number(timeArr[1]));
+    secondsLeft--;
+
+    timeArr[0] = parseInt(secondsLeft / 60);
+    timeArr[1] = parseInt(secondsLeft % 60);
+
+    timeArr[0] = timeArr[0] < 10 ? '0' + timeArr[0] : timeArr[0];
+    timeArr[1] = timeArr[1] < 10 ? '0' + timeArr[1] : timeArr[1];
+
+    return timeArr.join(':');
+  }
+
+  function startCountdown(timer) {
+    setInterval(function() {
+      timer.textContent = countdown(timer.textContent);
+    }, 1000)
+  }
+
+  function triggerStartBtnLabel(button) {
+    if (!button) {
+      return false;
+    }
+
+    button.textContent = button.textContent === 'Start' ? 'Pause' : 'Start';
   }
 });
