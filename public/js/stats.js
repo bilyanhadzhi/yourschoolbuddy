f1fc1feaf79197b7b7ed758b01d78b714f6597b2
Bilyan Hadzhi <bilyanhadzhi@gmail.com>
2017-08-03 16:50:00 +0300

    Configure bar chart
    Need to fix my date ranges in the backend,
    add a little form to the stats to select
    pre-made ranges like 'last week', 'last month'
    and one for custom ranges.

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,8 +1,8 @@
 'use strict';
 
-(function() {
+document.addEventListener('DOMContentLoaded', function() {
   var url = '/api/stats';
-  var params = 'range_start=' + '2017-07-01' + '&range_end=' + '2017-08-01';
+  var params = 'range_start=' + '2017-07-26' + '&range_end=' + '2017-08-04';
 
   var request = new XMLHttpRequest();
 
@@ -22,9 +22,89 @@
         data.push(response[date]);
       });
 
-      console.log(labels, data);
+      createChart(labels, data, barChartOptions);
     }
   };
 
   request.send(params);
-})();
+
+  function decimalToMinutesAndSeconds(minutes) {
+    if (minutes < 0) {
+      return null;
+    }
+
+    var min = Math.floor(Math.abs(minutes));
+    var sec = Math.floor((Math.abs(minutes) * 60) % 60);
+
+    var formatted = min + (min === 1 ? ' minute' : ' minutes');
+
+    if (sec !== 0) {
+      formatted += ' and ' + sec;
+      formatted += sec === 1 ? ' second' : ' seconds';
+    }
+
+    return formatted;
+  }
+
+  var barChartOptions = {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        data: data,
+        label: 'Time studied',
+        backgroundColor: 'rgba(56,142,60, 0.7)',
+        borderColor: 'rgba(56,142,60, 0.9)',
+        borderWidth: 1,
+      }],
+    },
+    options: {
+      tooltips: {
+        callbacks: {
+          label: function(tooltipItem, data) {
+            return decimalToMinutesAndSeconds(tooltipItem.yLabel);
+          },
+        },
+      },
+      legend: {
+        display: false,
+      },
+      responsive: true,
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            callback: function(label, index, labels) {
+              if (Math.floor(label) === label) {
+                return label;
+              } else {
+                return '';
+              }
+            },
+            fontFamily: '"Nunito", sans-serif',
+            fontSize: 13,
+          }
+        }],
+        xAxes: [{
+          gridLines: {
+            display: false,
+          },
+          ticks: {
+            callback: function(label, index, labels) {
+              return moment(label).format("MMM Do");
+            },
+            fontFamily: '"Nunito", sans-serif',
+            fontSize: 13,
+            fontStyle: 'bold',
+          }
+        }],
+      },
+    },
+  };
+
+  function createChart(labels, data, options) {
+    var context = document.getElementById('time-studied-bar-chart').getContext('2d');
+
+    var barChart = new Chart(context, options);
+  }
+});
--- src/views/stats.php
+++ src/views/stats.php
@@ -3,12 +3,20 @@
 <div class="container">
   <?php require_once('includes/flash_box.php') ?>
 
-  Stats are currently a work in progress.
+
+  <section class="section-container">
+    <h2 class="green">Time studied (minutes)</h2>
+    <section class="chart-container">
+      <canvas id="time-studied-bar-chart" width="500" height="200"></canvas>
+    </section>
+  </section>
+
 </div>
 
 <?php require_once('includes/timer.php') ?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
 <script src="/js/stats.js"></script>
 
 <?php require_once('includes/footer.php') ?>
