fc561fe219bf8d362b50bf5cc40dee8cd54b5655
Bilyan Hadzhi <bilyanhadzhi@gmail.com>
2017-08-21 22:16:03 +0300

    Add favicons

--- src/views/includes/header.php
+++ src/views/includes/header.php
@@ -4,8 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <?php require_once('favicons.php') ?>
   <link rel="stylesheet" href="/css/style.css">
-  <title><?=isset($title) ? $title : 'YourSchoolBuddy'?></title>
+  <title><?=isset($title) ? 'YourSchoolBuddy – ' . $title : 'YourSchoolBuddy'?></title>
 </head>
 <body>
-  <?php require('navbar.php'); ?>
+  <?php require_once('navbar.php') ?>
